0c20bc6a7f3b43f3b9a5f6312fd0560f48cee0e3
Chanthy Ngin <nginchanthy@gmail.com>
2017-12-16 20:44:11 -0800

    Passing information between Business and BusinessList

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import Business from './components/Business/Business.js';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+let businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
 export default class App extends Component {
   render() {
@@ -13,7 +35,7 @@ export default class App extends Component {
         <h1>ravenous</h1>
 
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses = {businesses} />
       </div>
     );
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,16 +4,14 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
   render(){
+
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        { 
+          this.props.businesses.map(business => {
+            return (<Business business = {business} />);
+          }) 
+        }   
       </div>
       );
   }
